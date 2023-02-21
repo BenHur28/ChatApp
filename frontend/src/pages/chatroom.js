df3000a3ee3cc4f01b0657b32882099327747318
Ben Hur <Bhur777@gmail.com>
2023-02-21 04:35:58 -0500

    add Websocket connection to backend server

---
+++ frontend/src/pages/chatroom.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { w3cwebsocket as W3CWebSocket } from "websocket";
+
+const Chatroom = () => {
+	const client = new W3CWebSocket("ws://127.0.0.1:8000/ws/chat/lobby/"); //gets room_name from the state and connects to the backend server
+
+	const onSubmit = () => {
+		client.send(
+			JSON.stringify({
+				message: "hello",
+			})
+		);
+	};
+
+	return (
+		<div>
+			chatroom
+			<button onClick={onSubmit} type="submit">
+				Send Hello
+			</button>
+		</div>
+	);
+};
+
+export default Chatroom;
