e4f0463f6bde285d9b07cc5cdbf2bdb45842019d
Ben Hur <Bhur777@gmail.com>
2023-02-23 01:49:27 -0500

    finish storing messages and rendering them on screen

--- frontend/src/pages/chatroom.js
+++ frontend/src/pages/chatroom.js
@@ -1,7 +1,7 @@
 import "../App.css";
 import React, { useState, useEffect } from "react";
 import { w3cwebsocket as W3CWebSocket } from "websocket";
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import Paper from "@mui/material/Paper";
@@ -10,64 +10,46 @@ import CardHeader from "@mui/material/CardHeader";
 
 const Chatroom = () => {
 	const { room_name } = useParams();
+	const location = useLocation();
+	const username = location.state.username;
 	const [message, setMessage] = useState(null);
-	const [msg, setMsg] = useState(null);
-	const [user, setUser] = useState(null);
+	const [messages, setMessages] = useState([]);
+	const client = new W3CWebSocket(`ws://127.0.0.1:8000/ws/chat/${room_name}/`);
 
 	useEffect(() => {
+		client.onopen = () => {
+			console.log("Websocket Client Connected");
+		};
 		client.onmessage = (event) => {
 			const data = JSON.parse(event.data);
-			console.log("message received");
-			console.log(data.message.value);
-			setMsg(data.message.value);
-			setUser("User1");
-			// const data = JSON.parse(event.data);
-			// document.querySelector("#chat-log").value += data.message + "\n";
+			if (data) {
+				console.log(messages);
+				setMessages((messages) => [...messages, { user: data.user, msg: data.message.value }]);
+			}
 		};
 	}, []);
 
-	const client = new W3CWebSocket(`ws://127.0.0.1:8000/ws/chat/${room_name}/`); //gets room_name from the state and connects to the backend server
-
-	// window.onload = () => {
-	// 	document.querySelector("#chat-message-submit").onclick = (event) => {
-	// 		const messageInputDom = document.querySelector(".chat-input");
-	// 		const message = messageInputDom.value;
-	// 		console.log(message);
-	// 		client.send(
-	// 			JSON.stringify({
-	// 				message: message,
-	// 			})
-	// 		);
-	// 	};
-	// };
-
 	const onButtonClicked = (e) => {
-		console.log("message sent");
-		console.log(message);
 		client.send(
 			JSON.stringify({
+				user: username,
 				message: message,
 			})
 		);
 	};
 
-	// client.onmessage = (event) => {
-	// 	console.log("message received");
-	// 	console.log(event.data);
-	// 	// const data = JSON.parse(event.data);
-	// 	// document.querySelector("#chat-log").value += data.message + "\n";
-	// };
+	let messageList = messages.map((user, index) => {
+		return <CardHeader key={index} title={user.user} subheader={user.msg} />;
+	});
 
 	return (
 		<div className="container">
 			<h2>Room Name:{room_name}</h2>
+			<h2>User name:{username}</h2>
 			<div className="chat-div">
 				<Paper variant="outlined">
-					<Card>
-						<CardHeader title={user} subheader={msg} />
-					</Card>
+					<Card>{messageList}</Card>
 				</Paper>
-				{/* <textarea disabled id="chat-log" cols="45" rows="30"></textarea> */}
 				<TextField
 					className="chat-input"
 					id="outlined-basic"
--- frontend/src/pages/lobby.js
+++ frontend/src/pages/lobby.js
@@ -6,13 +6,17 @@ const Lobby = () => {
 
 	const onSubmitHandler = () => {
 		const room = document.querySelector("#go-to-room");
+		const user = document.querySelector("#username");
 		const room_name = room.value;
-		navigate(`/chat/${room_name}`);
+		const username = user.value;
+		console.log(username);
+		navigate(`/chat/${room_name}`, { state: { username: username } });
 	};
 
 	return (
 		<div>
 			<input id="go-to-room" type="text"></input>
+			<input id="username" type="text"></input>
 			<button onClick={onSubmitHandler}>Go To Room</button>
 		</div>
 	);
